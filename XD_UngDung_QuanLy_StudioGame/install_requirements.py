eb04efb8ad7ab4c2b3e0c71e5ae6a915edf2b82b
Hieuhoang <hoanghieu2529@gmail.com>
2024-12-19 01:15:02 +0700

    v.0.4

---
+++ XD_UngDung_QuanLy_StudioGame/install_requirements.py
@@ -0,0 +1,27 @@
+import subprocess
+import sys
+import os
+
+def install_requirements():
+    """Tự động cài đặt các thư viện từ file requirements_library.txt"""
+    requirements_file = "requirements_library.txt"
+
+    # Đảm bảo  chạy lệnh này trong terminal trước khi cài bất cứ thư viện nào: python.exe -m pip install --upgrade pip
+    # Kiểm tra file requirements có tồn tại không ?
+    if not os.path.exists(requirements_file):
+        print(f"File {requirements_file} không tồn tại! Vui lòng kiểm tra lại.")
+        sys.exit(1)
+
+    print("Đang kiểm tra và cài đặt các thư viện cần thiết...")
+    try:
+        # Lệnh cài đặt các thư viện từ file requirements
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "pip"])
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", requirements_file])
+        print("Cài đặt thư viện thành công!")
+    except subprocess.CalledProcessError:
+        print("Có lỗi xảy ra trong quá trình cài đặt các thư viện!")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    install_requirements()
